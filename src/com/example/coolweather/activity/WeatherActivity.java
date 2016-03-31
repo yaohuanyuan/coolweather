61991c306965f197cc35168c9adb29418de00bdd
ahuan <273677125@qq.com>
2016-03-31 22:57:23 +0800

    增加后台自动更新数据

--- src/com/example/coolweather/activity/WeatherActivity.java
+++ src/com/example/coolweather/activity/WeatherActivity.java
@@ -3,6 +3,7 @@ package com.example.coolweather.activity;
 import java.security.PublicKey;
 
 import com.example.coolweath.R;
+import com.example.coolweather.service.AutoUpdateService;
 import com.example.coolweather.util.HttpCallbackListener;
 import com.example.coolweather.util.HttpUtil;
 import com.example.coolweather.util.Utility;
@@ -155,6 +156,8 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 	
 }
